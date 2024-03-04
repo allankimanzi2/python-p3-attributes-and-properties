16cec40a1045480aff5364b79fa8ba7672483886
allankimanzi2 <allankim457@gmail.com>
2024-03-04 12:04:20 +0300

    Done assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,21 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+   
+    def __init__(self,name = "",breed="Mutt"):
+        if not isinstance(name,str) :
+            print("Name must be string between 1 and 25 characters.")
+        elif name == "":
+             print("Name must be string between 1 and 25 characters.")
+        elif len(name)>25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+            if breed != "":
+                
+                if breed not in APPROVED_BREEDS:
+                    print("Breed must be in list of approved breeds.")
+                    self.breed = "Mutt"
+                else:
+                    self.breed = breed
+             
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,22 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self,name = "",job=""):
+        if not isinstance(name,str) :
+            print("Name must be string between 1 and 25 characters.")
+        elif name == "":
+             print("Name must be string between 1 and 25 characters.")
+        elif len(name)>25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name.title()
+        if job != "":
+            
+            if job not in APPROVED_JOBS:
+                print("Job must be in list of approved jobs.")
+                self.job = None
+            else:
+                self.job = job
+
+class Person:
     pass
